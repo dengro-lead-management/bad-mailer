5dc96db459425bf1854056d66e65ef21a2b7ea08
Dan Davidson <dan@rallyagency.co.uk>
2018-08-10 16:00:34 +0100

    Adding mock exception response (400)

--- src/BadMailerTransport.php
+++ src/BadMailerTransport.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Session;
 use Swift_Mime_Message;
 use Illuminate\Mail\Transport\Transport;
 use Illuminate\Support\Facades\Log;
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Psr7\Response;
 
 class BadMailerTransport extends Transport {
 
@@ -13,6 +16,14 @@ class BadMailerTransport extends Transport {
      * {@inheritdoc}
      */
     public function send(Swift_Mime_Message $message, &$failedRecipients = null) {
-        return null;
+        $client = new Client([
+            'handler' => MockHandler::createWithMiddleware([
+                new Response(400, [], "[MOCK ERROR] 'to' parameter is not a valid address. please check documentation")
+            ]),
+        ]);
+
+        $response = $client->post('/messages.mime');
+
+        return $response->getBody();
     }
 }
